cf51ac00184318911f38bf1992928045ae0ce4aa
Hammurabi <koberstine@hotmail.com>
2021-09-22 15:57:13 -0400

    added get_me querry and remove_book mutation in savedbooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,7 +69,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({variables: {input: bookToSave}});
+      const response = await saveBook({
+        variables: {input: bookToSave}
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
