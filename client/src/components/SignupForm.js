1f549e2fab85c09a1f901d038b3777139b66fe19
Hammurabi <koberstine@hotmail.com>
2021-09-23 20:40:41 -0400

    replaced server auth.js with functioning code

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
@@ -29,10 +29,10 @@ const SignupForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
-
+    
     try {
       const { data } = await addUser({
-        variables: { ...userFormData },
+        variables: { ...userFormData }
       });
 
       Auth.login(data.addUser.token);
